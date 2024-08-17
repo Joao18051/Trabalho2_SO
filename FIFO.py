f4d7170992d0d736e6666020278b779ae98e05cb
Joao18051 <joao.magalhaes.s1805@gmail.com>
2024-08-17 15:37:41 -0300

    0.1

--- FIFO.py
+++ FIFO.py
@@ -4,9 +4,6 @@ class FIFO(molduras, chamadaPaginas):
         self.chamadaProcessos = chamadaPaginas
 
         self.faltasPagina = 0
-        #Matriz vazia de 1 coluna e 4 linhas vazias, fazer cada linha ter tamanho de 8 colunas, 
-        #para representar os 8 ciclos de clock de observação, preenche-los com 0s.
-        self.contador = [[[] for _ in range(molduras)]]
 
     def start(self):
         #Criar molduras
